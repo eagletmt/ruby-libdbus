ceba33e715c7eb367f5060b4d86a2148bfdbfa6c
Kohei Suzuki <eagletmt@gmail.com>
2014-05-19 01:22:58 +0900

    Add Connection#register_object_path

--- ext/libdbus/connection.c
+++ ext/libdbus/connection.c
@@ -4,7 +4,15 @@ static VALUE cConnection;
 
 typedef struct {
   DBusConnection *conn;
+  VALUE callbacks;
 } wrapper_t;
+
+static void
+wrapper_mark(void *wrapper)
+{
+  wrapper_t *w = wrapper;
+  rb_gc_mark(w->callbacks);
+}
 
 static void
 wrapper_free(void *wrapper)
@@ -18,7 +26,8 @@ s_alloc(VALUE klass)
 {
   wrapper_t *w = ALLOC(wrapper_t);
   w->conn = NULL;
-  return Data_Wrap_Struct(klass, NULL, wrapper_free, w);
+  w->callbacks = rb_hash_new();
+  return Data_Wrap_Struct(klass, wrapper_mark, wrapper_free, w);
 }
 
 static DBusConnection *
@@ -112,6 +121,92 @@ m_request_name(VALUE self, VALUE name)
   return INT2NUM(result_code);
 }
 
+static void
+add_callback(VALUE conn, VALUE path, VALUE callback)
+{
+  wrapper_t *w;
+  Data_Get_Struct(conn, wrapper_t, w);
+  rb_hash_aset(w->callbacks, path, callback);
+}
+
+static VALUE
+get_callback(VALUE conn, VALUE path)
+{
+  wrapper_t *w;
+  Data_Get_Struct(conn, wrapper_t, w);
+  return rb_hash_aref(w->callbacks, path);
+}
+
+static void
+remove_callback(VALUE conn, VALUE path)
+{
+  wrapper_t *w;
+  Data_Get_Struct(conn, wrapper_t, w);
+  rb_hash_delete(w->callbacks, path);
+}
+
+typedef struct {
+  VALUE conn;
+  VALUE path;
+} vtable_user_data_t;
+
+static void
+on_unregister(DBusConnection *conn, void *user_data)
+{
+  vtable_user_data_t *data = user_data;
+
+  if (unwrap_connection(data->conn) != conn) {
+    rb_raise(rb_eRuntimeError, "Connection mismatch (internal error)");
+  }
+  remove_callback(data->conn, data->path);
+  xfree(data);
+}
+
+static void
+on_message(DBusConnection *conn, DBusMessage *message, void *user_data)
+{
+  vtable_user_data_t *data = user_data;
+  VALUE callback;
+
+  if (unwrap_connection(data->conn) != conn) {
+    rb_raise(rb_eRuntimeError, "Connection mismatch (internal error)");
+  }
+  callback = get_callback(data->conn, data->path);
+  rb_funcall(callback, rb_intern("call"), 1, libdbus_wrap_message(message));
+}
+
+static VALUE
+m_register_object_path(VALUE self, VALUE path)
+{
+  DBusError err = DBUS_ERROR_INIT;
+  DBusObjectPathVTable vtable;
+  VALUE callback;
+  dbus_bool_t ok;
+  vtable_user_data_t *data;
+
+  if (!rb_block_given_p()) {
+    rb_raise(rb_eArgError, "Requires block");
+  }
+  callback = rb_block_proc();
+  vtable.message_function = on_message;
+  vtable.unregister_function = on_unregister;
+  data = ALLOC(vtable_user_data_t);
+  data->conn = self;
+  data->path = path;
+  ok = dbus_connection_try_register_object_path(
+      unwrap_connection(self),
+      StringValueCStr(path),
+      &vtable,
+      data,
+      &err);
+  if (!ok) {
+    libdbus_raise_error(&err);
+  }
+  add_callback(self, path, callback);
+
+  return Qnil;
+}
+
 void
 Init_libdbus_connection(VALUE mLibDBus)
 {
@@ -125,4 +220,5 @@ Init_libdbus_connection(VALUE mLibDBus)
   rb_define_method(cConnection, "send_message", m_send_message, 1);
   rb_define_method(cConnection, "send_message_with_reply", m_send_message_with_reply, 1);
   rb_define_method(cConnection, "request_name", m_request_name, 1);
+  rb_define_method(cConnection, "register_object_path", m_register_object_path, 1);
 }
