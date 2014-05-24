fbb1446b84ac261d2c68711739088de705313ba7
Kohei Suzuki <eagletmt@gmail.com>
2014-05-24 13:46:19 +0900

    examples/server-call.rb: Show method and arguments

--- examples/server-call.rb
+++ examples/server-call.rb
@@ -4,15 +4,16 @@ Bundler.setup
 require 'libdbus'
 
 conn = LibDBus::Connection.session_bus
-p conn.request_name('cc.wanko.example1')
+conn.request_name('cc.wanko.example1')
 conn.register_object_path('/') do |message|
-  puts "On /"
+  puts "On /: #{message.interface}.#{message.member}"
+  p message.arguments
   reply = LibDBus::MethodReturnMessage.new(message)
   conn.send_message(reply)
   true
 end
 
 loop do
-  p conn.read_write_dispatch(0)
+  conn.read_write_dispatch(0)
   sleep 1
 end
