60242bcb79bc3eb2cbb888494a8176110e87dbb5
Kohei Suzuki <eagletmt@gmail.com>
2014-05-24 15:08:55 +0900

    Improve examples

--- examples/client-call.rb
+++ examples/client-call.rb
@@ -1,15 +1,23 @@
 #!/usr/bin/env ruby
+require 'bundler'
+Bundler.setup
 require 'pp'
 require 'libdbus'
 
-conn = LibDBus::Connection.system_bus
+conn = LibDBus::Connection.session_bus
 message = LibDBus::MethodCallMessage.new(
-  'org.freedesktop.systemd1',
-  '/org/freedesktop/systemd1/unit/ntpd_2eservice',
-  'org.freedesktop.DBus.Properties',
-  'GetAll',
+  'cc.wanko.example1',
+  '/',
+  'test.method.Type',
+  'Method',
 )
 p [message.destination, message.path, message.interface, message.member]
-message.add_argument('s', 'org.freedesktop.systemd1.Unit')
+message.add_argument('s', 'foo')
+message.add_argument('i', 42)
+message.add_argument('aab', [[true, true, false]])
+message.add_argument('d', 3.14)
+message.add_argument('o', '/object/path')
+message.add_argument('g', 'g')
 reply = conn.send_message_with_reply(message)
 pp reply.arguments
+LibDBus.validate_path!('/object/path')
--- examples/server-call.rb
+++ examples/server-call.rb
@@ -8,12 +8,18 @@ conn.request_name('cc.wanko.example1')
 conn.register_object_path('/') do |message|
   puts "On /: #{message.interface}.#{message.member}"
   p message.arguments
-  reply = LibDBus::MethodReturnMessage.new(message)
-  conn.send_message(reply)
-  true
+  case message.interface
+  when 'test.method.Type'
+    case message.member
+    when 'Method'
+      reply = LibDBus::MethodReturnMessage.new(message)
+      reply.add_argument('s', message.arguments.inspect)
+      conn.send_message(reply)
+    end
+  end
 end
 
 loop do
   conn.read_write_dispatch(0)
-  sleep 1
+  sleep 0.1
 end
